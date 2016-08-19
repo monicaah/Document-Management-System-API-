418248707ddb750eeb140892ed339658bb3beef8
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-08-19 10:11:00 +0300

    Add controller file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('Index');
-});
+const test = require('../controllers/index');
+
+router.get('/', test.index);
 
 module.exports = router;

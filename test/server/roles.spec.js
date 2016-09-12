6c2df99daa2858cdf04f4f96c7750e18fee358f1
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-09-13 02:50:16 +0300

    Layout for roles.spec file

---
+++ test/server/roles.spec.js
@@ -0,0 +1,36 @@
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const api = require('../../server');
+// const mongoose = require('mongoose');
+//
+// const User = mongoose.model('Users');
+// const expect = chai.expect;
+//
+// chai.use(chaiHttp);
+//
+//
+// describe('ROLES', () => {
+//   beforeEach(done => {
+//     done();
+//   });
+//
+//   it('POST /roles/ create role', done => {
+//     done();
+//   });
+//
+//   it('GET /roles/: Returns all roles.', done => {
+//     done();
+//   });
+//
+//   it('UPDATE /roles/<id>: Update document attributes.', done => {
+//     done();
+//   });
+//
+//   it('validates that a new role created has a unique title.', done => {
+//     done();
+//   });
+//
+//   it('DELETE /roles/<id>: Delete roles.', done => {
+//     done();
+//   });
+// });
